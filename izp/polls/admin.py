a994c26c3da3b497d4326678c804f55ccb25b4da
czapiga <Sz.Czapiga@gmail.com>
2017-10-22 01:21:13 +0200

    Added:   Template/Dummy voting application   .gitignore with few rules fixing git-python-pycharm workflow

---
+++ izp/polls/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
