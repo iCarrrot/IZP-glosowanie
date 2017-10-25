679a8ca5ac92442f00b7ba2c4abd06e3e221f3c5
czapiga <Sz.Czapiga@gmail.com>
2017-10-25 20:56:35 +0200

    Changed application localization to polish. Added few TODO marks in code.

--- izp/polls/models.py
+++ izp/polls/models.py
@@ -4,9 +4,9 @@ import datetime
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    access_codes = ['AAA', 'BBB', 'CCC'] #TODO generate random codes
+    question_text = models.CharField('Treść pytania', max_length=200)
+    pub_date = models.DateTimeField('Data publikacji')
+    access_codes = ['AAA', 'BBB', 'CCC']  # TODO generate random codes
 
     def __str__(self):
         return self.question_text
@@ -17,10 +17,17 @@ class Question(models.Model):
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    was_published_recently.short_description = 'Opublikowane niedawno?'
 
-    def is_correct_code(self, code):
+    def is_code_correct(self, code):
         return code in self.access_codes
+
+
+# TODO Create SimpleQuestion class (derived from Question) with predefined, fixed set of choices - (Yes/No)
+# TODO Create OpenQuestion class (derived from Question) with no predefined choices
+
+# TODO I think we need to keep track of all votes with information about vote date and used access code. Maybe it would
+#      be nice to introduce Vote model, with foreign keys to Question and Choice.
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- izp/polls/views.py
+++ izp/polls/views.py
@@ -12,10 +12,6 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
@@ -26,9 +22,6 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
@@ -37,35 +30,33 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
         print(request.POST['code'])
-        if request.POST['code'] == "" or not question.is_correct_code(request.POST['code']):
+        if request.POST['code'] == "" or not question.is_code_correct(request.POST['code']):
             raise AttributeError
 
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "Nie wybrano odpowiedzi",
         })
-    except (AttributeError):
+
+    except AttributeError:
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "Invalid access code",
+            'error_message': "Niewłaściwy kod uwierzytelniający",
         })
+
     else:
+        # TODO if someone voted using the same access code, we have to invalidate last vote (just remove 1 from last
+        # choice .votes because we want to keep all the votes in database)
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
