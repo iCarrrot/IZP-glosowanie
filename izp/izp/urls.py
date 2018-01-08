66520e1ae2475d8ffd453cd2074dacf4dc02e13f
sgorawski <sgorawski@gmail.com>
2018-01-08 20:02:16 +0100

    Added url for index that redirects to polls (#39)

--- izp/izp/urls.py
+++ izp/izp/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda _: HttpResponseRedirect('/polls/')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
